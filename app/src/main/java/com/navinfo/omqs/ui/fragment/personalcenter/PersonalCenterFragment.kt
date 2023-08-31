aad0197f5cbd5344f2afd4b0f3dce482ec341262
xiaoyan <xiaoyan159@163.com>
2023-08-31 15:06:05 +0800

    fix: 优化人行横道和文字渲染样式

--- app/src/main/java/com/navinfo/omqs/ui/fragment/personalcenter/PersonalCenterFragment.kt
+++ app/src/main/java/com/navinfo/omqs/ui/fragment/personalcenter/PersonalCenterFragment.kt
@@ -129,7 +129,7 @@ class PersonalCenterFragment(private var indoorDataListener: ((Boolean) -> Unit?
                     // 定位到指定位置
                     niMapController.mMapView.vtmMap.animator()
 //                        .animateTo(GeoPoint( 40.05108004733645, 116.29187746293708    ))
-                        .animateTo(GeoPoint( 40.50716396962102, 115.80381067125272))
+                        .animateTo(GeoPoint( 40.480633792652746, 115.99816629572948 ))
                 }
                 R.id.personal_center_menu_open_all_layer -> {
                     MapParamUtils.setDataLayerEnum(DataLayerEnum.SHOW_ALL_LAYERS)
