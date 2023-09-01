8a1d36b117e98f1a55b0a06de5aab8e145c4eae2
xiaoyan <xiaoyan159@163.com>
2023-09-01 10:34:58 +0800

    fix: 完善人行道对象面纹理图片

--- app/src/main/java/com/navinfo/omqs/ui/fragment/personalcenter/PersonalCenterFragment.kt
+++ app/src/main/java/com/navinfo/omqs/ui/fragment/personalcenter/PersonalCenterFragment.kt
@@ -129,7 +129,7 @@ class PersonalCenterFragment(private var indoorDataListener: ((Boolean) -> Unit?
                     // 定位到指定位置
                     niMapController.mMapView.vtmMap.animator()
 //                        .animateTo(GeoPoint( 40.05108004733645, 116.29187746293708    ))
-                        .animateTo(GeoPoint( 40.480633792652746, 115.99816629572948 ))
+                        .animateTo(GeoPoint( 40.503796123773355,115.81649866261817))
                 }
                 R.id.personal_center_menu_open_all_layer -> {
                     MapParamUtils.setDataLayerEnum(DataLayerEnum.SHOW_ALL_LAYERS)
