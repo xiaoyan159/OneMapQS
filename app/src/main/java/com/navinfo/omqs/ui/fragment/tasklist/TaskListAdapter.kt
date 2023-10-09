92ae4af99eab4873ab43bed9206a59e1849202c7
qiji4215 <qiji4215@navinfo.com>
2023-10-09 14:18:31 +0800

    merge code

--- app/src/main/java/com/navinfo/omqs/ui/fragment/tasklist/TaskListAdapter.kt
+++ app/src/main/java/com/navinfo/omqs/ui/fragment/tasklist/TaskListAdapter.kt
@@ -220,6 +220,7 @@ class TaskListAdapter(
                 binding.taskUploadBtn.isEnabled = false
                 binding.taskUploadBtn.setProgress(0)
                 binding.taskUploadBtn.setBackgroundColor(binding.root.resources.getColor(R.color.gray_121))
+                Log.e("qj","${taskBean.id}===changeUploadTxtViews===已上传")
             }
 
             FileUploadStatus.ERROR -> {
@@ -234,6 +235,7 @@ class TaskListAdapter(
             FileUploadStatus.NONE -> {
                 binding.taskUploadBtn.setText("未上传")
                 binding.taskUploadBtn.setProgress(0)
+                Log.e("qj","${taskBean.id}===changeUploadTxtViews===未上传")
             }
 
             FileUploadStatus.WAITING -> {
