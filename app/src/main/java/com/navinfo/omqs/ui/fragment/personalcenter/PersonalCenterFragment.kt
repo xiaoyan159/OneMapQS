fb85eebc872ef83f00cdd7173069196e8ebfea4f
xiaoyan <xiaoyan159@163.com>
2023-12-04 11:02:21 +0800

    fix: 修改测试定位位置

--- app/src/main/java/com/navinfo/omqs/ui/fragment/personalcenter/PersonalCenterFragment.kt
+++ app/src/main/java/com/navinfo/omqs/ui/fragment/personalcenter/PersonalCenterFragment.kt
@@ -196,7 +196,7 @@ class PersonalCenterFragment(private var indoorDataListener: ((Boolean) -> Unit?
                     // 定位到指定位置
                     niMapController.mMapView.vtmMap.animator()
 //                        .animateTo(GeoPoint( 40.05108004733645, 116.29187746293708    ))
-                        .animateTo(GeoPoint(40.07290793293324, 116.24617660398738 ))
+                        .animateTo(GeoPoint(39.88707004725549, 116.16390557370013))
                 }
 
                 R.id.personal_center_menu_open_all_layer -> {
